7472299cd43de5009ba8e407873a4e456deda758
Dialla Coulibaly <diallacoulibaly@gmail.com>
2026-05-24 22:51:05 +0000

    ajout de l'implementation du model

---
+++ src/main/java/model/ServiceImplemente/DomaineImplement.java
@@ -0,0 +1,47 @@
+package main.java.model.ServiceImplemente;
+
+import main.java.model.classes.Domaine;
+import main.java.model.dao.DomaineDao;
+import main.java.model.service.DomaineService;
+
+import java.util.List;
+
+public class DomaineImplement implements DomaineService {
+    public final DomaineDao domaineDao;
+    public DomaineImplement(DomaineDao domaineDao){
+        this.domaineDao=domaineDao;
+    }
+    @Override
+    public void ajouter(String domaine){
+        if(domaine==null||domaine.trim().isEmpty()){
+            System.out.println("nom du domaine obligatoire");
+            return;
+        }
+        Domaine d=new Domaine(domaine);
+        domaineDao.ajoutDomaine(d);
+
+
+
+
+    }
+    @Override
+    public void modifier(int id,String domaine){
+        if(id<0){
+            System.out.println("id obligatoire");
+            return;
+        }
+        Domaine d=new Domaine(id,domaine);
+        domaineDao.modifierDomaine(d);
+    }
+    @Override
+    public void supprimer(int id) {
+        domaineDao.supprimerDomaine(id);
+    }
+
+    @Override
+    public List<Domaine> afficher() {
+        return domaineDao.afficherDomaine();
+    }
+
+
+}
---
+++ src/main/java/model/classes/Domaine.java
@@ -0,0 +1,30 @@
+package main.java.model.classes;
+
+public class Domaine {
+
+    private int id;
+    private  String domaine;
+    public  Domaine(Integer id, String domaine){
+        this.id=id;
+        this.domaine=domaine;
+    }
+    public Domaine(){}
+    public  Domaine(String domaine){
+        this.domaine=domaine;
+
+    }
+    //les getters
+    public int getId(){
+        return id;
+    }
+    public String  getDomaine(){
+        return domaine;
+    }
+    //les setters
+    public void  setId(int id){
+        this.id=id;
+    }
+    public  void setDomaine(String domaine){
+        this.domaine=domaine;
+    }
+}
