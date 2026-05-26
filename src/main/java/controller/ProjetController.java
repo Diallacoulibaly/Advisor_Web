e27d237e40811d6589c6ad40b403c1bb15f9c165
sey-y-s <seybayacoub@gmail.com>
2026-05-26 17:02:00 +0000

    Fixing bugs AGAINNN*

--- src/main/java/controller/ProjetController.java
+++ src/main/java/controller/ProjetController.java
@@ -6,55 +6,99 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import main.java.model.ServiceImplemente.ProjetServiceImpl;
 import main.java.model.dao.ProjetDao;
 import main.java.model.enums.Niveau;
 import main.java.model.DaoImplement.ProjetDaoImpl;
 import main.java.model.classes.Projet;
+import main.java.model.service.ProjetService;
 
 import java.io.IOException;
-import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @WebServlet("/projets")
 public class ProjetController extends HttpServlet {
+    private ProjetService service;
 
-    // private ProjetDao projetDAO;
+    public void init() {
+        ProjetDao p = new ProjetDaoImpl();
+        service = new ProjetServiceImpl(p);
+    }
 
-    // @Override
-    // public void init() throws ServletException {
-    //     this.projetDAO = new ProjetDaoImpl();
-    // }
 
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) 
-            throws ServletException, IOException {
-        
-        // try {
-        //     List<Projet> listeProjets = projetDAO.findAll();
-        //     request.setAttribute("projets", listeProjets);
-        //     request.getRequestDispatcher("/WEB-INF/view/projets.jsp").forward(request, response);
-            
-        // } catch (SQLException e) {
-        //     throw new ServletException("Erreur lors de la récupération des projets", e);
-        // }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        int id;
+        String action = request.getParameter("action");
+        if (action == null) action = "list";
 
-        List<Projet> listeProjets = new ArrayList<>();
-
-        Projet p1 = new Projet();
-        p1.setTitre("Agriculture Urbaine");
-        p1.setBudgetMax(50000.0);
-        
-        Projet p2 = new Projet();
-        p2.setTitre("Digitalisation PME");
-        p2.setBudgetMax(250000.0);
-        
-        listeProjets.add(p1);
-        listeProjets.add(p2);
-
-        request.setAttribute("projets", listeProjets);
-    
-        request.getRequestDispatcher("/WEB-INF/view/projets.jsp").forward(request, response);
-
+        switch (action) {
+            case "create":
+                request.getRequestDispatcher("/WEB-INF/view/formProjet.jsp").forward(request, response);
+                break;
+            case "edit":
+                id = Integer.parseInt(request.getParameter("id"));
+                Optional<Projet> projet = service.getProjetById(id);
+                request.setAttribute("projet", projet);
+                request.getRequestDispatcher("/WEB-INF/view/formProjet.jsp").forward(request, response);
+                break;
+            case "delete":
+                id = Integer.parseInt(request.getParameter("id"));
+                service.supprimerProjet(id);
+                response.sendRedirect("projets");
+                break;
+            default:
+                List<Projet> liste = service.getAllProjets();
+                request.setAttribute("projets", liste);
+                request.getRequestDispatcher("/WEB-INF/view/projets.jsp").forward(request, response);
+                break;
+        }
     }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        String action = request.getParameter("action");
+        if ("save".equals(action)) {
+            enregistrerProjet(request, response);
+        }
+    }
+
+    private void enregistrerProjet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        try {
+            String titre = request.getParameter("titre");
+            String description = request.getParameter("description");
+            float duree = Float.parseFloat(request.getParameter("duree"));
+            String niveau = request.getParameter("niveau");
+            double budgetMin = Double.parseDouble(request.getParameter("budgetMin"));
+            double budgetMax = Double.parseDouble(request.getParameter("budgetMax"));
+
+            Projet p = new Projet();
+
+            p.setTitre(titre);
+            p.setDescription(description);
+            p.setDuree(duree);
+            p.setNiveau(Niveau.valueOf(niveau));
+            p.setBudgetMin(budgetMin);
+            p.setBudgetMax(budgetMax);
+
+            service.enregistrerProjet(p);
+            response.sendRedirect("projets");
+
+        } catch (IllegalArgumentException e) {
+            // plus tard : utiliser requestDispatcher pour afficher e
+            response.sendRedirect("projets?action=create&erreur=donnees_invalides");
+        }
+    }
+
 }
+
+
+
+
+
+
+
+
