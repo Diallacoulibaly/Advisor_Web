64c376392d0b358684e6fb2c90d6045c3170db9f
iamabdoulaziz <abdoulazizc867@gmail.com>
2026-06-08 17:00:34 +0000

    Transfert marquerTerminer in depensesController, deleted ActiviteClientController

--- src/main/java/controller/DepenseController.java
+++ src/main/java/controller/DepenseController.java
@@ -1,9 +1,12 @@
 package main.java.controller;
 
+import main.java.model.DaoImplement.ActiviteClientDaoImplement;
+import main.java.model.ServiceImplemente.ActiviteClientServiceImplement;
 import main.java.model.classes.Activite;
 import main.java.model.classes.Client;
 import main.java.model.classes.Depense;
 import main.java.model.ServiceImplemente.DepenseImplement;
+import main.java.model.dao.ActiviteCloentDao;
 import main.java.model.dao.DepenseDao;
 import main.java.model.DaoImplement.DepenseDaoImplement;
 import main.java.model.service.DepenseService;
@@ -21,12 +24,16 @@ import java.util.Optional;
 public class DepenseController extends HttpServlet {
 
     private DepenseService depenseService;
+    private ActiviteClientServiceImplement activiteClientServiceImplement;
 
     @Override
     public void init() {
         // CORRECTION: instanciation correcte du DAO et du service
         DepenseDao depenseDao = new DepenseDaoImplement();
         depenseService = new DepenseImplement(depenseDao);
+
+        ActiviteCloentDao activiteClientDao = new ActiviteClientDaoImplement();
+        activiteClientServiceImplement = new ActiviteClientServiceImplement(activiteClientDao);
     }
 
     @Override
@@ -94,6 +101,9 @@ public class DepenseController extends HttpServlet {
                     }
 
                     depenseService.add(depense);
+                    assert activiteIdParam != null;
+                    activiteClientServiceImplement.marquerTerminer(Integer.parseInt(activiteIdParam));
+
                 } catch (Exception e) {
                     request.setAttribute("erreur", "Données invalides : " + e.getMessage());
                     request.getRequestDispatcher("/WEB-INF/view/pages/activite.jsp").forward(request, response);
